c26d58737d4432cdd61c6e74cde541167d520be1
Justas0628 <101471436+Justas0628@users.noreply.github.com>
2025-01-16 16:21:02 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '"hacked by atlastoby" - emilis';
+    : '"AtlasToby.fun - Geriausias Minecraft serveris';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'AtlasToby.fun - Geriausias Minecraft serveris';
